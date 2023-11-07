0a6e3fcb44363568153bd47cf90aefcb4d046522
Akip2 <126561427+Akip2@users.noreply.github.com>
2023-11-07 16:06:03 +0100

    Mise à jour creation_tables

--- creation_tables.sql
+++ creation_tables.sql
@@ -21,7 +21,7 @@ id INT(4) NOT NULL AUTO_INCREMENT,
 text varchar(235),
 id_image int(3),
 id_auteur int(3) NOT NULL,
-datePubli date,
+datePubli datetime,
 Constraint primary key(id),
 foreign key(id_image) references image(id),
 foreign key(id_auteur) references utilisateur(id)
