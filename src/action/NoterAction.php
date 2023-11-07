fa70fd856716e7420bfb269602e721caadd8c2da
RobinSlimani <RobinSlimani>
2023-11-07 14:46:06 +0100

    Action de notation fini normalement :)

--- src/action/NoterAction.php
+++ src/action/NoterAction.php
@@ -1,14 +1,32 @@
 <?php
 namespace iutnc\touiter\action;
+use iutnc\touiter\utilisateur\Utilisateur;
 class NoterAction extends Action {
     public function execute() : string{
         if($this->http_method === "GET"){
-            $note = Utilisateur::verifierAvis()
-            if(){
-
+            $note = Utilisateur::verifierAvis($_GET["idTouite"]);
+            if($note !== null){
+                if($note > 0){
+                    $page = "<form id=\"noter\" method=\"POST\" action=\"?action=NoterAction\"/>
+                    <input type='radio' name='choix' value='Dislike'>
+                    <button type=\"submit\">Valider</button>
+                    </form>";
+                }else{
+                    $page = "<form id=\"noter\" method=\"POST\" action=\"?action=NoterAction\"/>
+                    <input type='radio' name='choix' value='Like'>
+                    <button type=\"submit\">Valider</button>
+                    </form>";
+                }  
+            }else{
+                "<form id=\"noter\" method=\"POST\" action=\"?action=NoterAction\"/>
+                            <input type='radio' name='choix' value='Like'>
+                            <input type='radio' name='choix' value='Dislike'>
+                            <button type=\"submit\">Valider</button>
+                            </form>";
             }
 		}
 		elseif($this->http_method === "POST"){
+            $page = "<h1> Vous avez donné votre avis merci !</h1>";
 		}
 		return $page;
     }
