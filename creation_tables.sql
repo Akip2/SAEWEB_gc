7ee55f87dce7515df4c982e3bc217a2dba2dc68d
Lucoss <lucoss@debian.home>
2023-11-07 09:05:45 +0100

    ajout modification de la bd

--- creation_tables.sql
+++ creation_tables.sql
@@ -1,6 +1,6 @@
 Create Table utilisateur (
 id INT(3) NOT NULL AUTO_INCREMENT,
-nom varchar(20), 
+nom varchar(20),
 prenom varchar(20),
 mail varchar(40),
 motdepasse varchar(20),
@@ -8,30 +8,34 @@ role int(2),
 Constraint primary key(id)
 );
 
-Create Table touite(
-id INT(4) NOT NULL AUTO_INCREMENT ,
-text varchar(235),
-image varchar(60),
-id_auteur int(3) NOT NULL,
-note int(3),
-datePubli date,
-Constraint primary key(id),
-foreign key(id_auteur) references utilisateur(id)
-);
 
 Create Table image(
 id INT(3) NOT NULL AUTO_INCREMENT,
 descImage varchar(500),
-chemin varchar(60), 
+chemin varchar(60),
 Constraint primary key(id)
 );
 
+
+Create Table touite(
+id INT(4) NOT NULL AUTO_INCREMENT,
+text varchar(235),
+id_image int(3) not null,
+id_auteur int(3) NOT NULL,
+datePubli date,
+Constraint primary key(id),
+foreign key(id_image) references image(id),
+foreign key(id_auteur) references utilisateur(id)
+);
+
+
 Create Table tag(
-id INT(3) NOT NULL AUTO_INCREMENT, 
+id INT(3) NOT NULL AUTO_INCREMENT,
 libelle varchar(30),
 descTag varchar(50),
 Constraint primary key(id)
 );
+
 
 Create Table touite2tag(
 id_touite int(3) not null,
@@ -41,7 +45,8 @@ foreign key(id_touite) references touite(id),
 foreign key(id_tag) references tag(id)
 );
 
-Create Table abonnement(
+
+Create Table suivreUtilisateur(
 id_suiveur int(3) not null,
 id_suivit int(3) not null,
 Constraint primary key(id_suiveur,id_suivit),
@@ -49,10 +54,21 @@ foreign key(id_suiveur) references utilisateur(id),
 foreign key(id_suivit) references utilisateur(id)
 );
 
-Create Table suivre(
+
+Create Table suivreTag(
 id_suiveur int(3) not null,
 id_tag int(3) not null,
 Constraint primary key(id_suiveur,id_tag),
 foreign key(id_suiveur) references utilisateur(id),
 foreign key(id_tag) references tag(id)
+);
+
+
+Create Table evaluation(
+id_touite int(3) not null,
+id_utilisateur int(3) not null,
+note int(1),
+Constraint primary key(id_touite,id_utilisateur),
+foreign key(id_touite) references touite(id),
+foreign key(id_utilisateur) references utilisateur(id)
 );
