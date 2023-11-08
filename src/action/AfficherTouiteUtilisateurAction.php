34dc2e8ad15739537e93fa9d0c71b05e04f09042
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2023-11-08 20:02:25 +0100

    modification affichage profil utilisateur

--- src/action/AfficherTouiteUtilisateurAction.php
+++ src/action/AfficherTouiteUtilisateurAction.php
@@ -9,12 +9,22 @@ class AfficherTouiteUtilisateurAction extends Action{
         $id = $_GET["id"];
         $listeTouite = ListeTouite::listeTouiteUser($id);
         $rl = new RenderListe($listeTouite);
-        $html = $rl->render(1);
+        
         if(isset($_SESSION['user'])){
             $user = unserialize($_SESSION['user']);
             if($user->id === intval($id)){
+                $html = "<h2>Bienvenue sur votre profil ".$user->prenom." ".$user->nom."</h2>";
+                $html .= $rl->render(1);
                 $html .= Utilisateur::utilisateurNarcissique();     
             }
+            else{
+                $html = "<h2>Bienvenue sur le profil</h2><br>";
+                $html .= $rl->render(1);
+            }
+        }
+        else{
+            $html = "<h2>Bienvenue sur le profil</h2><br>";
+            $html .= $rl->render(1);
         }
         return $html;
     }
