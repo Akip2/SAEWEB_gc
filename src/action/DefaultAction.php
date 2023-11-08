71fcffe2120763d72f3de03031b1e61b7958a1a1
Akip2 <126561427+Akip2@users.noreply.github.com>
2023-11-08 17:15:14 +0100

    mise a joure default

--- src/action/DefaultAction.php
+++ src/action/DefaultAction.php
@@ -7,17 +7,17 @@ class DefaultAction extends Action{
         $option = ""; 
         if (isset($_SESSION["user"])) {   //Si authentifié
             $u = unserialize($_SESSION["user"]);
-            $option = "<a href=\"index.php?action=publish_touite\">Publier un touite</a><br>
-            <a href=\"index.php?action=list_touite_utilisateur&id=$u->id\">Profil</a><br>
-            <a href=\"index.php?action=follow_tag\">Suivre des tags</a><br>
+            $option = "<br><a href=\"index.php?action=publish_touite\">Publier un touite</a>
+            <a href=\"index.php?action=list_touite_utilisateur&id=$u->id\">Profil</a>
+            <a href=\"index.php?action=follow_tag\">Suivre des tags</a>
             <a href=\"index.php?action=deconection\">Se déconnecter</a><br>";
         }
         else{
-            $option = "<a href=\"index.php?action=inscription\">Inscription</a><br>
-                <a href=\"index.php?action=connexion\">Connexion</a><br>
-                <a href=\"index.php?action=list_touite\">Lister les Touites</a><br>
-                <a href=\"index.php?action=list_user_touite\">Touites d'une Personne</a><br>
-                <a href=\"index.php?action=list_tag_touite\">Touites d'un tag</a><br>";
+            $option = "</br><a href=\"index.php?action=inscription\">Inscription</a>
+                <a href=\"index.php?action=connexion\">Connexion</a>
+                <a href=\"index.php?action=list_touite\">Lister les Touites</a>
+                <a href=\"index.php?action=list_user_touite\">Touites d'une Personne</a>
+                <a href=\"index.php?action=list_tag_touite\">Touites d'un tag</a></br>";
         }
         return "<h1>Touiter</h1>
         <h2>Bienvenue sur Touiter!</h2>
