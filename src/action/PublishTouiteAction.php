d39d2ed8abcf6dcb660fbece9bf958fdd7493473
Akip2 <126561427+Akip2@users.noreply.github.com>
2023-11-09 17:21:36 +0100

    Creation d'une limite à 235 caracteres dans le textarea de publishtouite

--- src/action/PublishTouiteAction.php
+++ src/action/PublishTouiteAction.php
@@ -13,8 +13,9 @@ class PublishTouiteAction extends Action {
         if($this->http_method==="GET"){
             $contenu="
             <h3>Publier un touite</h3>
+            <p>(limite de 235 caractères)</p>
             <form method='post' enctype='multipart/form-data' action='?action=publish_touite'>
-                <textarea name=\"texte\" rows=\"4\" cols=\"50\" placeholder=\"Votre touite\" required></textarea> 
+                <textarea maxlength='235' name=\"texte\" rows=\"4\" cols=\"50\" placeholder=\"Votre touite\" required></textarea> 
                 <input class=\"bouton\" type='submit' value='Publier'><br>
                 <div class=\"labelUpload\">
                     <label for=\"upload\">Mettre une image dans le touite : </label>
@@ -27,33 +28,28 @@ class PublishTouiteAction extends Action {
         else if($this->http_method==="POST"){
 
             $texte=filter_var($_POST["texte"], FILTER_DEFAULT);
-            if(strlen($texte)<=235){
-                //date de publication 
-                date_default_timezone_set('Europe/Paris');
-                $date_publication = date('Y-m-d H:i:s');
-                //print($date_publication);
+            //date de publication 
+            date_default_timezone_set('Europe/Paris');
+            $date_publication = date('Y-m-d H:i:s');
+            //print($date_publication);
                 
-                $touite; //Initialisation du touite
+            $touite; //Initialisation du touite
 
-                $dest=null;
-                if($_FILES["image"]["tmp_name"]!=null){
+            $dest=null;
+            if($_FILES["image"]["tmp_name"]!=null){
 
-                    $tmp=$_FILES["image"]["tmp_name"];
+                $tmp=$_FILES["image"]["tmp_name"];
 
-                    $filename=uniqid();
+                $filename=uniqid();
                     
-                    $dest="img/$filename.png"; //Stockage de l'image côté serveur
+                $dest="img/$filename.png"; //Stockage de l'image côté serveur
 
-                    move_uploaded_file($tmp, $dest);
-                }
+                move_uploaded_file($tmp, $dest);
+            }
 
-                //Insertion du touite
-                $touite=Touite\Touite::insererCreer($texte, $date_publication, $dest);
-                $contenu="<b>Touite publié avec succès!</b></p>";
-            }
-            else{
-                $contenu="<b>Touite trop long !</b></br>";
-            }
+            //Insertion du touite
+            $touite=Touite\Touite::insererCreer($texte, $date_publication, $dest);
+            $contenu="<b>Touite publié avec succès!</b></p>";}
         }
 
         return $contenu;
